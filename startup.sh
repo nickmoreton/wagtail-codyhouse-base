80df29bc07ce829fffb61380d61386f92c7cc462
Nick Moreton <nickmoreton@me.com>
2020-09-11 16:41:39 +0100

    first commit

---
+++ startup.sh
@@ -0,0 +1,27 @@
+
+#!/bin/bash
+
+# gets codyhouse-framework and extract
+wget https://github.com/CodyHouse/codyhouse-framework/archive/master.zip && unzip master.zip \
+&&  cd codyhouse-framework-master \
+&& rm .gitignore && rm gulpfile.js && rm LICENSE.md && rm README.md \
+&& mkdir main/assets/js/compnents \
+&& cd .. \
+&& mv codyhouse-framework-master/** . \
+&& rm -R codyhouse-framework-master \
+&& rm master.zip \
+
+# set up wagtail project in root
+python -m venv venv && source venv/bin/activate \
+&& pip install wagtail \
+&& wagtail start config . \
+&& python manage.py migrate \
+&& echo "from django.contrib.auth import get_user_model; get_user_model().objects.create_superuser('admin', '', 'changeme')" | python3 manage.py shell \
+&& pip freeze > requirements.txt \
+&& rm .dockerignore && rm Dockerfile \
+
+# install dev dependancies
+npm install
+
+# final cleanup
+rm LICENSE && rm README.md
